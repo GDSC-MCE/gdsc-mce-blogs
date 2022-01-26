26662c64e6571d31a12dddc4051b182e83ef7046
Visakh47 <visakhbobby47@gmail.com>
2022-01-26 15:16:31 +0530

    prefer lq js render + meta query + config changes

--- site.config.js
+++ site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // open graph metadata (optional)
   description: 'Google Developer Student Club MCE is a community for the students interested in Google developer technologies. Students from any course with an interest in growing as a developer are welcome. By joining a GDSC MCE, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.',
   socialImageTitle: 'GDSC MCE',
-  socialImageSubtitle: 'Hello World! 👋',
+  socialImageSubtitle: 'Follow our socials!',
 
   // social usernames (optional)
   twitter: 'gdsc_mce',
